76963e66756ccd1115e95c80644a8ef1d044e613
dasWarder <Al.94_94@mail.ru>
2021-10-18 16:09:41 +0300

    Add dependencies, configuration for a balancer

---
+++ src/main/java/com/example/kafkagatewaybalancerstreams/KafkaGatewayBalancerStreamsApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafkagatewaybalancerstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGatewayBalancerStreamsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaGatewayBalancerStreamsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/kafkagatewaybalancerstreams/KafkaGatewayBalancerStreamsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.kafkagatewaybalancerstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGatewayBalancerStreamsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
